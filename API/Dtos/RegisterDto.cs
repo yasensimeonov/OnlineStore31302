076c3b388a02e7f559b71bdcad0cd9b7def69bf4
Yasen Simeonov <yasen@formenn.is>
2020-12-31 13:06:55 +0200

    End of Section 16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage = "Passwords must have 1 Uppercase, 1 Lowercase, 1 number and 1 non alphanumeric and at least 6 characters")]
         public string Password { get; set; }
     }
 }
