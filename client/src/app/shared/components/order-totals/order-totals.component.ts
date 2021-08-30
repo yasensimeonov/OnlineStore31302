9219a373d899b01a91b251fc0fdce01298fa8b99
Yasen Simeonov <yasen@formenn.is>
2021-08-30 16:10:57 +0300

    Section 20 (stretch challenge) completed - shared order-totals and basket-summary between basket, checkout-review and order-details.

--- client/src/app/shared/components/order-totals/order-totals.component.ts
+++ client/src/app/shared/components/order-totals/order-totals.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasketTotals } from '../../models/basket';
@@ -9,12 +9,17 @@ import { IBasketTotals } from '../../models/basket';
   styleUrls: ['./order-totals.component.scss']
 })
 export class OrderTotalsComponent implements OnInit {
-  basketTotal$: Observable<IBasketTotals>;
+  // basketTotal$: Observable<IBasketTotals>;
 
-  constructor(private basketService: BasketService) { }
+  @Input() shippingPrice: number;
+  @Input() subTotal: number;
+  @Input() total: number;
+
+  // constructor(private basketService: BasketService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.basketTotal$ = this.basketService.basketTotal$;
+    // this.basketTotal$ = this.basketService.basketTotal$;
   }
 
 }
